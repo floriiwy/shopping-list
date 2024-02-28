cbf8cfb374c2781d42d078ba9191f062bdaf18f4
floriiwy <ai3007st@gmail.com>
2024-02-28 13:42:15 +0300

    modified:   index.html modified:   script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const messages = document.querySelector('.items');
+const sendInput = document.querySelector('#input-wrapper');
 
+sendInput.addEventListener('keydown', function() {
+    const messageText = sendInput.value;
+
+    const newMessage = document.createElement('p');
+    newMessage.classList.add('message');
+    newMessage.textContent = messageText;
+
+    if (messageText != '') {
+        messages.append(newMessage);
+    }
+
+    sendInput.value = '';
+});
 /* Пустые элементы не должны добавляться */
 
 /* Если кликнуть на элемент списка, он зачеркивается */
